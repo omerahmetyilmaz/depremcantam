2a277e523c97807ad903b8398529c46b0c8f0bb9
omerahmetyilmaz <omramt54@gmail.com>
2025-12-14 16:56:32 +0300

    PWA fixes manifest

---
+++ public/sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'deprem-cantasi-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/bag-transparent.png'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Return cached version or fetch from network
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+            )
+    );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
